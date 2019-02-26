6151895a0576abc675badde54ef1be99620573f9
Targaryen Egg <919617516@qq.com>
2019-02-26 17:49:06 +0800

    添加Header并进行了初步的设计

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="header-wrap">
+        <el-menu
+                :default-active="activeIndex"
+                class="el-menu-demo"
+                mode="horizontal"
+                background-color="#DCDCDC"
+                text-color="#000"
+                active-text-color="#000000">
+            <img id="logo" src="../assets/logo.png" height="40px" width="40px">
+            <el-button id="logoin" type="info" plain>登录</el-button>
+            <el-button id="register" type="info" plain>注册</el-button>
+            <el-menu-item index="1">
+                <router-link :to="{name : 'home'}">主页</router-link>
+            </el-menu-item>
+            <el-menu-item index="2">
+                <router-link :to="{name : 'about'}">关于</router-link>
+            </el-menu-item>
+        </el-menu>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        data() {
+            return {
+                activeIndex: '1'
+            };
+        }
+    }
+</script>
+
+<style lang="stylus">
+    #logo {
+        float left
+        padding 10px
+    }
+
+    #logoin {
+        float right
+        margin 10px
+    }
+
+    #register {
+        float right
+        margin 10px
+    }
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
