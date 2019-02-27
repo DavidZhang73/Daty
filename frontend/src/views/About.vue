1d98f7f63450dbc7249e577797ea0d2790cc84f0
Targaryen Egg <919617516@qq.com>
2019-02-27 22:54:51 +0800

    About页面动工

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="about-wrap">
-        <h1>This is an about page</h1>
+        <el-row class="about-row1">
+            <el-card :body-style="{ padding: '0px' }" class="row1-card1" shadow="always">
+                <img src="../assets/DavidBill-github.png" class="image">
+                <div class="card1-text">
+                    <el-row>
+                        <span style="font-size: 20px;">DavidBill</span>
+                    </el-row>
+                    <el-row>
+                        <el-button type="text" style="font-size: 14px;" align="right">Github</el-button>
+                    </el-row>
+                </div>
+            </el-card>
+        </el-row>
     </div>
 </template>
 
@@ -15,6 +27,22 @@
 <style lang="stylus">
     .about-wrap {
         height 100%
+        margin-left 150px
 
+        .about-row1 {
+
+            .row1-card1 {
+                width 236px
+
+                .image {
+                    width 100%
+                    display block
+                }
+
+                .card1-text {
+                    width 100%
+                }
+            }
+        }
     }
 </style>
