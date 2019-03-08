38c541fe76690aaec8c636ecf1b1d067de9a265e
EggTargaryen <34475415+dzxrly@users.noreply.github.com>
2019-03-08 20:24:32 +0800

    Merge branches 'Egg' and 'david' of https://github.com/DavidZhang73/daty into Egg
    # Conflicts:
    #	frontend/src/App.vue
    #	frontend/src/assets/logo_svg.svg
    #	frontend/src/components/Footer.vue
    #	frontend/src/components/Header.vue
    #	frontend/src/components/UserBackground.vue
    #	frontend/src/router.js
    #	frontend/src/views/About.vue
    #	frontend/src/views/Home.vue
    #	frontend/src/views/NotFound.vue
    #	frontend/src/views/user/ForgetPassword.vue
    #	frontend/src/views/user/ForgetPasswordReset.vue
    #	frontend/src/views/user/Login.vue
    #	frontend/src/views/user/Signin.vue
    
    开始制作userPassword页面

--- frontend/src/views/center/UserPassword.vue
+++ frontend/src/views/center/UserPassword.vue
@@ -1,17 +1,51 @@
 <template>
     <div class="user-password-wrap">
-        userPassword
+        <div class="user-password">
+            <el-steps :active="active" finish-status="success">
+                <el-step title="步骤 1"></el-step>
+                <el-step title="步骤 2"></el-step>
+                <el-step title="步骤 3"></el-step>
+            </el-steps>
+
+            <div class="changePassword">
+                <div class="step-1">
+                    <p>Step 1</p>
+                    <el-input placeholder="Please input new password"
+                              v-model="firstInput"
+                              clearable=""></el-input>
+                </div>
+            </div>
+
+            <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "UserPassword"
+        name: "UserPassword",
+        data() {
+            return {
+                active: 0,
+                firstInput: '',
+            };
+        },
+        methods: {
+            next() {
+                if (this.active++ > 2) this.active = 0;
+            }
+        }
     }
 </script>
 
 <style lang="stylus">
     .user-password-wrap {
+        height 100%
+        width 100%
 
+        .user-password {
+            margin 0 auto
+            width 70%
+        }
     }
 </style>
