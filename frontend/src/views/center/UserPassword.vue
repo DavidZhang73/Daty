d79b682c48089aeb5419f0b4f0f3f927602caeca
EggTargaryen <34475415+dzxrly@users.noreply.github.com>
2019-03-10 16:58:19 +0800

    Merge branch 'david' of https://github.com/DavidZhang73/daty into Egg
    # Conflicts:
    #	frontend/src/components/Header.vue
    #	frontend/src/views/About.vue
    #	frontend/src/views/Home.vue
    
    "修改密码"页面样式基本完成

--- frontend/src/views/center/UserPassword.vue
+++ frontend/src/views/center/UserPassword.vue
@@ -8,15 +8,45 @@
             </el-steps>
 
             <div class="changePassword">
-                <div class="step-1">
-                    <p>Step 1</p>
-                    <el-input placeholder="Please input new password"
+                <div class="step-1" v-if="stepOneSeen">
+                    <el-input type="password"
+                              v-model="oldPassword"
+                              @blur="pwCheck"
+                              clearable>
+                        <template slot="prepend">请输入旧的密码</template>
+                    </el-input>
+                </div>
+                <div class="step-2" v-if="stepTwoSeen">
+                    <el-input type="text"
+                              placeholder="密码不能全为数字且应大于8位"
                               v-model="firstInput"
-                              clearable=""></el-input>
+                              clearable>
+                        <template slot="prepend">请输入新的密码</template>
+                    </el-input>
+                </div>
+                <div class="step-3" v-if="stepThreeSeen">
+                    <el-input type="text"
+                              placeholder="密码不能全为数字且应大于8位"
+                              v-model="secondInput"
+                              clearable>
+                        <template slot="prepend">请再次输入新的密码</template>
+                    </el-input>
                 </div>
             </div>
-
-            <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+            <el-button-group>
+                <el-button
+                        type="primary"
+                        icon="el-icon-arrow-left"
+                        @click="last"
+                        :disabled="canSeenLastBtn">上一步
+                </el-button>
+                <el-button
+                        type="primary"
+                        @click="next"
+                        :disabled="canSeenNextBtn">下一步<i
+                        class="el-icon-arrow-right el-icon--right"></i>
+                </el-button>
+            </el-button-group>
         </div>
     </div>
 </template>
@@ -27,12 +57,64 @@
         data() {
             return {
                 active: 0,
+                oldPassword: '',
                 firstInput: '',
+                secondInput: '',
+                canSeenLastBtn: true,
+                canSeenNextBtn: false,
+                stepOneSeen: true,
+                stepTwoSeen: false,
+                stepThreeSeen: false
             };
         },
         methods: {
             next() {
-                if (this.active++ > 2) this.active = 0;
+                this.active++;
+            },
+            last() {
+                this.active--;
+            },
+            pwCheck() {
+                
+            },
+        },
+        watch: {
+            active(newVal) {
+                console.log('watch: ' + newVal);
+                if (newVal === 1) {
+                    this.canSeenNextBtn = false;
+                    this.canSeenLastBtn = false;
+
+                    this.stepOneSeen = false;
+                    this.stepTwoSeen = true;
+                    this.stepThreeSeen = false;
+                } else if (newVal === 2) {
+                    this.canSeenNextBtn = false;
+                    this.canSeenLastBtn = false;
+
+                    this.stepOneSeen = false;
+                    this.stepTwoSeen = false;
+                    this.stepThreeSeen = true;
+                } else if (newVal === 3) {
+                    this.canSeenNextBtn = true;
+                    this.canSeenLastBtn = false;
+
+
+                } else if (newVal === 0) {
+                    this.canSeenNextBtn = false;
+                    this.canSeenLastBtn = true;
+
+                    this.stepOneSeen = true;
+                    this.stepTwoSeen = false;
+                    this.stepThreeSeen = false;
+                } else {
+                    this.canSeenNextBtn = false;
+                    this.canSeenLastBtn = false;
+
+                    this.stepOneSeen = true;
+                    this.stepTwoSeen = false;
+                    this.stepThreeSeen = false;
+                }
             }
         }
     }
@@ -40,12 +122,33 @@
 
 <style lang="stylus">
     .user-password-wrap {
+        margin-top 50px
         height 100%
         width 100%
 
         .user-password {
             margin 0 auto
             width 70%
+
+            .changePassword {
+                margin-top 40px
+
+                .step-1 {
+                    width 70%
+                }
+
+                .step-2 {
+                    width 70%
+                }
+
+                .step-3 {
+                    width 70%
+                }
+            }
+
+            .el-button-group {
+                margin-top 30px
+            }
         }
     }
 </style>
