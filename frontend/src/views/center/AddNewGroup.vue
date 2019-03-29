9636809894d27d221f699787b9052430820c339c
EggTargaryen <34475415+dzxrly@users.noreply.github.com>
2019-03-29 15:52:43 +0800

    开始编写新建用户组页面

--- frontend/src/views/center/AddNewGroup.vue
+++ frontend/src/views/center/AddNewGroup.vue
@@ -1,5 +1,21 @@
 <template>
-    
+    <el-row class="addNewGroup-wrap">
+        <el-col :xs="{span:24}"
+                :sm="{span:20}"
+                :lg="{span:16}">
+            <el-form class="addNewGroup-form" label-width="100px">
+                <el-form-item prop="userGroupName" label="用户组名称">
+                    <el-input
+                            type="text"
+                            placeholder="请输入用户组名称">
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="userList" label="用户组成员">
+
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
@@ -9,5 +25,12 @@
 </script>
 
 <style lang="stylus">
+    .addNewGroup-wrap {
 
+        .addNewGroup-form {
+
+            .el-input {
+            }
+        }
+    }
 </style>
