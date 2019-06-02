d6aa4ec03fe06ae191bb07450e248ec2668a0a2b
David <davidbill@outlook.com>
2019-06-02 11:31:02 +0800

    add file api url

--- daty/apiurls.py
+++ daty/apiurls.py
@@ -2,8 +2,11 @@ from django.urls import path, include
 
 app_name = 'api'
 
+from utils.views import UploadFileAPI
+
 urlpatterns = [
     path('usergroup/', include('usergroup.urls', namespace='usergroup')),
     path('user/', include('user.urls', namespace='user')),
-    path('collection/', include('collection.urls', namespace='collection'))
+    path('collection/', include('collection.urls', namespace='collection')),
+    path('file/', UploadFileAPI.as_view())
 ]
