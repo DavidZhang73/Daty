38e339fc1f94d654d5a822b7f3b747b548d6744e
David <davidbill@outlook.com>
2019-06-03 20:19:29 +0800

    add collection file download all api

--- collection/urls.py
+++ collection/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from utils.views import DownloadAllCollectionFileAPI
 
 app_name = 'collection'
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('', views.CollectionAPI.as_view()),
     path('<uuid:pk>/', views.CollectionDetailAPI.as_view()),
     path('file/', views.CollectionFileAPI.as_view()),
-    path('file/<int:pk>/', views.CollectionFileUploadAPI.as_view())
+    path('file/<int:pk>/', views.CollectionFileUploadAPI.as_view()),
+    path('file/all/', DownloadAllCollectionFileAPI.as_view())
 ]
--- utils/views.py
+++ utils/views.py
@@ -1,9 +1,14 @@
 import time
+import zipfile
+from io import BytesIO
 
+from django.http.response import HttpResponse
 from rest_framework import permissions
 from rest_framework import views
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
+from collection.models import CollectionFile
 from utils.models import UploadFile
 
 
@@ -23,3 +28,21 @@ class UploadFileAPI(views.APIView):
             'id': f.id,
             'url': f.get_url()
         })
+
+
+class DownloadAllCollectionFileAPI(views.APIView):
+    def get(self, request):
+        collection_id = request.query_params.get('collection_id')
+        collection_file_list = CollectionFile.objects.filter(collection_id=collection_id)
+        file_list = []
+        try:
+            for collection_file in collection_file_list:
+                if collection_file.file:
+                    file_list.append(collection_file.file.file)
+            zip = BytesIO()
+            with zipfile.ZipFile(zip, 'w', zipfile.ZIP_DEFLATED) as zf:
+                for file in file_list:
+                    zf.writestr(str(file), file)
+        except Exception as e:
+            raise APIException(e)
+        return HttpResponse(zip.getvalue(), content_type="application/zip")
